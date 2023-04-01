a948a4847603940d7f2e9d70faab2cf7c8fd8adf
Abhishek C Sreejith <abhisheksreejithabhi673@gmail.com>
2023-04-01 18:03:09 +0530

    Echelon Events

---
+++ event.js
@@ -0,0 +1,250 @@
+$(".button-solid1").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">VIVA CARNIVAL </p><p>ICE BREAKER</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Ms. Pannaga - 9449962408</p><p id="rules">Rules: <br />Participants: All management event members except Best Manager Participant. Duration  3+1 MINUTE.Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">FORAS AVENGERS </p><p>MARKETING EVENT</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L403 </p><p>Student Coordinator:<br>Ms. Josna Pereira - 8861289674</p><p id="rules">Rules: <br />3 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid2").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">FIN FIESTA </p><p>FINANCE</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: 002 MB</p><p>Student Coordinator:<br>Ms. Bhavya-7022779666</p><p id="rules">Rules: <br />3 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid3").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">JINZAI</p><p>HR EVENT</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L404 </p><p>Student Coordinator:<br>Ms.  Thejaswini - 6363246118</p><p id="rules">Rules: <br />3 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid4").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">THE MONARCHS</p><p>BUSSINESS MANAGEMENT TEAM</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L102</p><p>Student Coordinator:<br>MS. Jishna M - 8139063933 </p><p id="rules">Rules: <br />4 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid5").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">ZEN MASTER</p><p>BEST MANAGER</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L102 </p><p>Student Coordinator:<br>Ms. Ashwini G-9591265688</p><p id="rules">Rules: <br />Participants: Individual event. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid6").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">FERRIS WHEEL</p><p>BUSINESS QUIZ</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L101 </p><p>Student Coordinator:<br>Mr. Sivaprasad s - 7356195833</p><p id="rules">Rules: <br />2 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid7").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">DRAGONS DEN</p><p>SPARK TANK</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L402 & Seminar Hall</p><p>Student Coordinator:<br>Ms. Ranjana Sonkar - 8296565854</p><p id="rules">Rules: <br />3 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+$(".button-solid8").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">KAIZEN</p><p>OPERATIONS</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location:L103</p><p>Student Coordinator:<br>Mr. Nandan Kumar d – 8129123831</p><p id="rules">Rules: <br />3 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid9").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">ESPALON</p><p>ANALYTICS</p><p id="date">Date: Day 1</p><p id="fee">Entry Fee: Rs 200/-</p><p id="location">Location: L501</p><p>Student Coordinator:<br>Ms. Divyashree M – 6238190533</p><p id="rules">Rules: <br />3 make a team. Any number of participants from a college can participate for the event. The event is open to both PG and UG students. Participants should carry laptops, net setters, pen drives etc. for their use. The organizers shall not be responsible for providing any of these items. Judges and the organizers decision will be final and binding. Any team found using unfair means shall be immediately disqualified. To keep your identity confidential, participants are supposed to use the team’s name allotted by Echelon team.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid26").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">FASHIDO </p><p>BE INGENIOUS</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Auditorium</p><p>Student Coordinator:<br>Ms. Kalpitha-6362205476</p><p id="rules">Rules: <br />Participants: 6 to 8 make a team. Duration: 10 Minutes. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid27").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">RANG FIESTA </p><p>BRAND RANGOLI</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: 6th floor New Building</p><p>Student Coordinator:<br>Ms. Anjitha K - 6282959778 </p><p id="rules">Rules: <br />Participants: 2 make a team. Duration: 1 hour. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid28").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">RUMBA</p><p>SPOT DANCE</p><p id="date">Date: Day 1 & 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Outdoor</p><p>Student Coordinator:<br>Ms. Ashrita - 8296144395</p><p id="rules">Rules: <br />Participants: Individual. Duration: 3+1 Minute. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid42").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">LA FIESTA </p><p>SPOT GAMES</p><p id="date">Date: Day 1 & 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Outdoors</p><p>Student Coordinator:<br>Mr. Mohammed Asman -9901251370</p><p id="rules">Rules: <br />Participants: Individual. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid38").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">LA CAZA </p><p>INFLUENCERS</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Outdoors</p><p>Student Coordinator:<br>Ms. Elizabeth Jasmine - 9008376676 </p><p id="rules">Rules: <br />Participants: 2 make a team. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid31").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">PANTO MIMER</p><p>MIME SHOW</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Auditorium</p><p>Student Coordinator:<br>Mr. Akshay -9048348213 </p><p id="rules">Rules: <br />Theme: Social Cause. Participants: 6 to 8 make a team. Time Limit: 8+2 Mins. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid39").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">SINFONIA </p><p>SINGING WITH INSTRUMENTS</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Edusat</p><p>Student Coordinator:<br>Ms. Malavika -9645476847   </p><p id="rules">Rules: <br />Participants: 2 Make a team. Time Limit: 4+1 Mins. Any number of participants from a college can participate for the events. The event is open to both PG and UG students.  </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid33").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">MUSCLE MANIA </p><p>BEST PHYSIQUE</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Auditorium</p><p>Student Coordinator:<br>Mr. Asheek Shetty - 6363387371</p><p id="rules">Rules: <br />Participants: Individual Event. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid34").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">EL CAMINO</p><p>MOVIE AUCTION</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: L103</p><p>Student Coordinator:<br>Mr. Nived - 7907144901  </p><p id="rules">Rules: <br />Participants: 2 make a team. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid35").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">MAGIC GALA</p><p>KHREDA KSHERA</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Outdoor</p><p>Student Coordinator:<br>Mr. Nithin - 8123919909</p><p id="rules">Rules: <br />Participants: 2 make a team. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid41").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">EA SPORTS </p><p>FIFA 23</p><p id="date">Date: Day 2</p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Lab 4th Floor</p><p>Student Coordinator:<br>Mr.  Mohd Faizan - 8861187281 </p><p id="rules">Rules: <br />Participants: 2 make a team. Any number of participants from a college can participate for the events. The event is open to both PG and UG students. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
