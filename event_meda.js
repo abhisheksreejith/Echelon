515dd4db71a176d1f6a9b82bdf00f9c3b0e11125
Abhishek C Sreejith <abhisheksreejithabhi673@gmail.com>
2023-04-02 12:34:53 +0530

    Medha events

---
+++ event_meda.js
@@ -0,0 +1,126 @@
+$(".button-solid1").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">MATRIX </p><p>CODING</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Ms. Pooja Dileep - 8129710797</p><p id="rules">Rules: <br />Participants: 2.  Total round: 3. Language: C, C++, Java. No Gadgets are allowed. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid2").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">ART OF DESIGN </p><p>WEB DESIGN</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Mr. Pavan Kumar K – 7090582452</p><p id="rules">Rules: <br /> Participants: 2. Total Round: 2. Language: HTML, CSS, JavaScript.</p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid3").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">CLASH OF TITAN </p><p>GAMING</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Mr. Rohan D Karkera – 8921237465</p><p id="rules">Rules: <br />Participants: 4. On premises. Bring your own phone and earphones. No tablets allowed.  </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+$(".button-solid").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">WOLF OF WALL STREET </p><p>IT MANAGER</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Ms. Shobitha Shetty - 8123070487</p><p id="rules">Rules: <br />Participant: 1. Rules and Rounds will be given on the spot. The Contestant must carry a laptop.  </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+$(".button-solid4").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">THE OFFICE  </p><p>TECH-TALK</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Ms. Swathi Y V – 7022415188</p><p id="rules">Rules: <br />Participant: 1. Rules and Round will be given on the spot. Round 1 and Round 2 . </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+$(".button-solid5").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">MIND HUNTER </p><p>QUIZ</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Mr. Sukesh - 6361776570</p><p id="rules">Rules: <br />Participants: 2.  No conferring will be allowed between teams during the Quiz. No Gadgets allowed. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+$(".button-solid6").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">FINDING NEMO </p><p>TREASURE HUNT</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Ms. Bhavanishesha S M - 7619269968</p><p id="rules">Rules: <br />Participants: 2. Round details will be given on the spot. Should not participate in any other event. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid7").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">LA LA LAND </p><p>DANCE</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Ms. Prathiksha H S – 6360292916</p><p id="rules">Rules: <br />Participants: Minimum 5 member.  Time Duration: Max 5 minutes. Music should be submitted in pen drive at least 1 hour before the competition. Props are allowed. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+$(".button-solid8").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">PIXELS </p><p>PHOTOGRAPHY</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Mr. Deepak CH – 8971290622</p><p id="rules">Rules: <br />Participants: 1. Participants should get their own laptop, camera and headphone. </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid9").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">THE REAL LIFE </p><p>VIDEOGRAPHY</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Mr. Vivek - 6361356901</p><p id="rules">Rules: <br />Participants: 2. Cover all activities of Sambhram and Medha. Should not participate in any other event. Participants should get their own gadgets.  </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
+
+$(".button-solid10").click(function() {
+    Swal.fire({
+        title: "Event Details",
+
+        html: '<div class="modalEventDetails"><p id="eventName">THE TERMINATORS </p><p>IOT EXHIBITION</p><p id="date">Date: Day 1 </p><p id="fee">Entry Fee: Rs 150/-</p><p id="location">Location: Seminar hall </p><p>Student Coordinator:<br>Mr. Deepak M - 9901939816</p><p id="rules">Rules: <br />Participants: Max 5 members. Theme: innovative.  </p><a class="reglink" href="http://register.shreedevisambhram.com">Register</a></div>',
+        showCloseButton: true,
+        icon: "",
+        showCancelButton: false,
+        showConfirmButton: false,
+    });
+});
